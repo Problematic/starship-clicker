a2e92eed24de15b3caf680fd1329022b31f5ce92
Derek Stobbe <djstobbe@gmail.com>
2015-02-02 00:27:58 -0700

    Shot flare on fire

--- src/js/entities/Starship.js
+++ src/js/entities/Starship.js
@@ -29,12 +29,19 @@ Starship.prototype.fire = function (target) {
     var projectile = this.projectiles.getFirstDead();
     var rotation = this.game.math.angleBetween(this.x, this.y, target.x, target.y);
     var sep = new Phaser.Point(this.x, this.y);
-    sep.rotate(sep.x, sep.y, rotation, false, 32);
+    sep.rotate(sep.x, sep.y, rotation, false, 16);
 
     projectile.rotation = rotation - projectile.rotationOffset;
     projectile.reset(sep.x, sep.y);
     this.game.physics.arcade.velocityFromRotation(rotation, this.game.config[this.shipType].projectileSpeed, projectile.body.velocity);
     this.nextShotAt = this.game.time.time + (1000 / this.game.config[this.shipType].rateOfFire);
+
+    var flare = this.game.add.sprite(sep.x, sep.y, 'sprites');
+    flare.anchor.setTo(0.5);
+    flare.scale.setTo(0.5);
+    var anim = flare.animations.add('fire', Phaser.Animation.generateFrameNames('Lasers/laser' + this.game.config[this.shipType].projectileColor, 8, 9, '', 2), 120);
+    anim.killOnComplete = true;
+    flare.animations.play('fire');
 };
 
 module.exports = Starship;
